26963ecf87f70b330b260ff9be9de07cd7359114
wangyukun <6475593@qq.com>
2015-04-23 00:28:23 +0800

    版本号就是版本号，别乱改

--- shenmesafe/src/main/java/kelaodi/shenmesafe/activity/SetupOneActivity.java
+++ shenmesafe/src/main/java/kelaodi/shenmesafe/activity/SetupOneActivity.java
@@ -3,7 +3,7 @@ package kelaodi.shenmesafe.activity;
 /**
  * Created by Administrator on 2015/4/23.
  */
-public class SetupOneActivity  extends BaseSetActivity{
+public class SetupOneActivity extends BaseSetActivity {
     @Override
     public void initView() {
 
